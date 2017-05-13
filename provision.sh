908678a305c2716ef8703ae05ef955cb2c60ddbc
Erlend Aakre <erlend@frostvoid.com>
2017-05-13 11:00:40 +0100

    added provisioning script file

---
+++ provision.sh
@@ -0,0 +1,16 @@
+# This script assumes you have docker installed and will download the frostvoid/mumble
+# image from docker hub, create a volume, start the container and set up murmur (mumble server).
+
+# Edit these 3 lines with your server/SuperUser password and MOTD (message shown to users when logging in)
+SERVERPASS="kf92IXoywOY3HdfALuOKmvTEH"
+ADMINPASS="gS2ICaf5oiTf1Ae0esCSUGWVQ"
+MOTD="<br/>core.frostvoid.com Mumble server<br/>Private!"
+
+# Optionally alter these if you want, but these defaults should be good for most uses
+CONTAINERNAME="mumble"
+VOLUMENAME="mumble-data"
+RESTARTPOLICY="--restart=always"
+
+docker pull frostvoid/mumble
+docker volume create $VOLUMENAME
+docker run -d -p 64738:64738 -p 64738:64738/udp -v $VOLUMENAME:/data --name=$CONTAINERNAME $RESTARTPOLICY -e "SERVER_PASSWORD=$SERVERPASS" -e "SUPW=$ADMINPASS" -e "SERVER_TEXT=$MOTD" frostvoid/mumble
