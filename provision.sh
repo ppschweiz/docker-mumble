b29f3db5c4eb59fd5a828ad82a6f0db963d1d587
Erlend Aakre <erlend@frostvoid.com>
2017-05-13 11:55:47 +0000

    ffs docker, just build this image!

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 # This script assumes you have docker installed and will download the frostvoid/mumble
 # image from docker hub, create a volume, start the container and set up murmur (mumble server).
 
-# IMPORTANT!
+# IMPORTANT
 # Edit these 3 lines with your server/SuperUser password and MOTD (message shown to users when logging in)
 SERVERPASS="kf92IXoywOY3HdfALuOKmvTEH"
 ADMINPASS="gS2ICaf5oiTf1Ae0esCSUGWVQ"
